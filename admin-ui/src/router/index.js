f299bbdf037f978d77b913c9c63ac0f07af16944
jijianchun <jijianchun@kedacom.com>
2018-01-12 15:14:19 +0800

    rename home.vue

--- admin-ui/src/router/index.js
+++ admin-ui/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/page/Home'
+import Short from '@/page/Short'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'Short',
+      component: Short
     }
   ]
 })
